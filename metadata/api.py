330e7ebce0aba489b8394d9f2d9cee96740d0a81
Andrew Thomas <andr3w.l.thomas@gmail.com>
2014-11-09 19:34:39 -0500

    added login/logout support for backend

--- metadata/api.py
+++ metadata/api.py
@@ -2,9 +2,14 @@
 from tastypie.resources import Resource, ModelResource, ALL_WITH_RELATIONS, ALL
 from tastypie.authorization import DjangoAuthorization
 from metadata.models import *
-from django.contrib.auth.models import User
 from tastypie.serializers import Serializer
 from tastypie import fields
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from tastypie.http import HttpUnauthorized, HttpForbidden
+from django.conf.urls import url
+from tastypie.utils import trailing_slash
 
 from django.http.response import HttpResponse
 from tastypie import http
@@ -117,3 +122,43 @@ class UserResource(BaseCorsResource):
         queryset = User.objects.all()
         resource_name = 'user'
         serializer = Serializer(formats=['json'])
+
+    def override_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/login%s$" %   (self._meta.resource_name, trailing_slash()), self.wrap_view('login'), name="api_login"),
+            url(r'^(?P<resource_name>%s)/logout%s$' %  (self._meta.resource_name, trailing_slash()), self.wrap_view('logout'), name='api_logout'),
+        ]
+
+    def login(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
+
+        data = request.POST
+
+        username = data.get('username', '')
+        password = data.get('password', '')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return self.create_response(request, {
+                    'success': True
+                })
+            else:
+                return self.create_response(request, {
+                    'success': False,
+                    'reason': 'disabled',
+                    }, HttpForbidden )
+        else:
+            return self.create_response(request, {
+                'success': False,
+                'reason': 'incorrect',
+                }, HttpUnauthorized )
+
+    def logout(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        if request.user and request.user.is_authenticated():
+            logout(request)
+            return self.create_response(request, { 'success': True })
+        else:
+            return self.create_response(request, { 'success': False }, HttpUnauthorized)
