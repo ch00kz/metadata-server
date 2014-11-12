e9a47a2501d0c27950c00c750c7f01da2ce5fe6b
Andrew Thomas <andr3w.l.thomas@gmail.com>
2014-11-11 23:07:47 -0500

    started using postgresql

--- metadata/metadata/settings.py
+++ metadata/metadata/settings.py
@@ -61,8 +61,11 @@ LOGIN_URL = '/login/'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'metadata',                      
+        'USER': 'metadata',
+        'PASSWORD': 'metadata',
+        'HOST': 'localhost'
     }
 }
 
