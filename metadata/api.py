3daab4884187d9aacf66e2daefeff664d922159d
Andrew Thomas <andr3w.l.thomas@gmail.com>
2014-12-18 00:12:58 -0500

    added custom endpoint to create project (need to complete)

--- metadata/api.py
+++ metadata/api.py
@@ -1,3 +1,7 @@
+import json
+from django.http import HttpResponse
+from django.conf.urls import url
+from tastypie.utils import trailing_slash
 from tastypie.resources import ModelResource
 from tastypie.serializers import Serializer
 from metadata.models import Staff, Project
@@ -16,6 +20,44 @@ class ProjectResource(ModelResource):
 		resource_name = "project"
 		allowed_methods = ['get', 'post']
 		serializer = Serializer(formats=['json'])
+
+	def prepend_urls(self):
+		return [
+			url(r"^(?P<resource_name>%s)/create%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('create_project'), name="create_project"),
+		]
+
+	def create_project(self, request, **kwargs):
+		if request.POST:
+			data = json.loads(request.POST['data'])
+			name = data.get("name","")
+			status = data.get("status","")
+			category = data.get("category","")
+			start_date = data.get("start_date","")
+			expected_end_date = data.get("expected_end_date","")
+			end_date = data.get("end_date","")
+			value = data.get("value","")
+			clients = data.get("clients","")
+			departments = data.get("department","")
+			currency = data.get("currency","")
+			lead_staff = data.get("lead_staff","")
+
+			error = None
+			if not name:
+				error = "Please give the project a name"
+			elif not category:
+				error = "Please select a category"
+			elif not status:
+				error = "Please select a status"
+			elif value and not currency:
+				error = "Please select a currency"
+			elif not departments:
+				error = "Please select a department"
+			elif not lead_staff:
+				error = "Please select a lead staff member"
+
+			if error:
+				return HttpResponse(json.dumps(dict(status = False, msg = error)), content_type="application/json")
+		return HttpResponse(json.dumps(dict(status = True, project_name = name)), content_type="application/json")
 
 	def dehydrate(self, bundle):
 		obj = bundle.obj
